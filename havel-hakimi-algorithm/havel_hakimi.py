ae29a168dcc98725d58e39bcb57ee779e2b20faa
allliyah <74038500+allliyah@users.noreply.github.com>
2021-10-16 00:46:46 +0800

    Update havel_hakimi.py
    - Remove comment
    = Rename slice_sequence to slice

--- havel-hakimi-algorithm/havel_hakimi.py
+++ havel-hakimi-algorithm/havel_hakimi.py
@@ -1,16 +1,3 @@
-"""
-Algorithm
-1. Arrange degree sequence in descending order
-2. Identify how many nodes are to be selected using the first node. If 0:
-   -- Check if the elements of the degree sequence are all zeroes. If so, degree sequence is graphical [exit]. 
-
-If > 0:
-3. Remove the first node of the degree sequence
-4. Check if the length of the updated degree sequence is less than or equal to the number of selected nodes. If not, degree sequence is not graphical [exit].
-5. Subtract 1 from the selected nodes if the previous condition is true. 
-6. Repeat
-"""
-
 class DegreeSequence:
 
     def __init__(self, degree_seq):
@@ -31,10 +18,13 @@ class DegreeSequence:
         for node in range(num_selected):
             self.degree_seq[node] -= 1
     
-    def slice_sequence(self):
+    def slice(self):
         """ Removes the first node of the sorted degree sequence"""
         self.degree_seq = self.degree_seq[1:]
 
     def sort(self):
         """ Sorts the degree seqeuence in descending order"""
         self.degree_seq.sort(reverse = True)
+
+    def __str__(self):
+        return "".join(str(self.degree_seq))
